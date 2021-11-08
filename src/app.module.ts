d975d3791508d6709c57ae26f9781be5be7b9c6d
OG-Jons <jonas@marschall.pro>
2021-11-08 11:36:04 +0100

    setup for heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'ProjectTool2021Cool',
-      database: 'nestjs-tasks',
-      entities: [],
-      synchronize: true,
-    }),
-  ],
+  imports: [TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
